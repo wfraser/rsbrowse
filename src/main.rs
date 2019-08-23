538de5e47df0e837cfa11eba420c829530c1715b
William R. Fraser <wfraser@codewise.org>
2019-08-23 11:17:19 -0700

    add a --list-crates option

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ struct Arguments {
 
 fn usage() {
     eprintln!("usage: {} <crate name> <cargo workspace path>", std::env::args().next().unwrap());
+    eprintln!("    or specify '--list-crates' for <crate name> to list all crates in the workspace");
 }
 
 fn parse_args() -> Option<Arguments> {
@@ -51,13 +52,25 @@ fn main() {
     eprintln!("Reading analysis data...");
     let analysis = Analysis::load(&args.crate_path);
 
+    if args.crate_name == "--list-crates" {
+        for c in analysis.crates() {
+            println!("{}", c.name);
+        }
+        return;
+    }
+
     let crate_id = analysis.crates()
         .filter(|id| id.name == args.crate_name)
         .single()
-        .unwrap_or_else(|found|
-            panic!("{} instances of crates named {:?}",
+        .unwrap_or_else(|found| {
+            // TODO: should probably allow specifying the crate disambiguator to avoid this, but
+            // this seems like an uncommon case
+            eprintln!("{} instances of crates named {:?}",
                 if found { "multiple" } else { "no" },
-                args.crate_name));
+                args.crate_name);
+            eprintln!("(this can sometimes be resolved by first doing a 'cargo clean' in the workspace)");
+            unimplemented!("support multiple crates with same name");
+        });
 
     println!("top-level definitions:");
     for def in analysis.defs(crate_id, "").unwrap() {
